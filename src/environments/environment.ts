bde8ef1364656b81fb2bfd30c59d9d59ab215949
475michal <166406013+475michalk@users.noreply.github.com>
2024-06-25 02:23:58 +0300

    real server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // apiURL: 'http://localhost:5000'
 
-    apiURL:'https://recipes-nodejs-backend-2.onrender.com'
+    apiURL:'https://recipes-nodejs-backend-5.onrender.com/'
 
 };
